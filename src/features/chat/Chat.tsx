28729914fa2cd3331a94599e0b4664d0f02a7037
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-03-19 15:59:18 +0100

    Resolve #24 - Implemented chat functionality

--- src/features/chat/Chat.tsx
+++ src/features/chat/Chat.tsx
@@ -1,41 +1,101 @@
+import { FormEvent, useEffect, useState } from "react";
+import {
+  createMessage,
+  createRun,
+  createThread,
+  getMessages,
+  isRunComplete,
+} from "../../services/OpenAiService";
 import "./Chat.css";
 import ChatMessage from "./ChatMessage";
 
 export default function Chat() {
+  const [threadId, setThreadId] = useState<string | null>(null);
+  const [userInput, setUserInput] = useState<string>("");
+  const [messages, setMessages] = useState<any[]>([]);
+
+  const fetchCreateThread = () => {
+    createThread().then((data) => {
+      setThreadId(data.data.id);
+      localStorage.setItem("threadId", data.data.id);
+      return data;
+    });
+  };
+
+  const wait = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+  const fetchCreateMessage = async (content: string, threadId: string) => {
+    createMessage(content, threadId).finally(() => fetchMessages(threadId));
+    const runId = (await createRun(threadId)).data.id;
+    let isComplete = (await isRunComplete(threadId, runId)) === "completed";
+    while (!isComplete) {
+      await wait(500);
+      if ((await isRunComplete(threadId, runId)) === "failed") break;
+      isComplete = (await isRunComplete(threadId, runId)) === "completed";
+    }
+    fetchMessages(threadId);
+  };
+
+  const fetchMessages = (threadId: string) => {
+    getMessages(threadId).then((data) => {
+      setMessages(data.data.data);
+    });
+  };
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!threadId) return;
+    const userMessage = {
+      content: [
+        {
+          text: {
+            value: "",
+          },
+        },
+      ],
+      role: "user",
+      thread_id: "",
+      created_at: 0,
+    };
+    setUserInput("");
+    setMessages([...messages, userMessage]);
+    fetchCreateMessage(userInput, threadId);
+  };
+
+  useEffect(() => {
+    const storedThreadId = localStorage.getItem("threadId");
+    if (!storedThreadId) {
+      fetchCreateThread();
+      return;
+    }
+    setThreadId(storedThreadId);
+    fetchMessages(storedThreadId);
+  }, []);
+
   return (
     <div className="container d-flex flex-column align-items-center w-100 gap-3">
       <ul className="chat__window">
-        <ChatMessage
-          isUser={true}
-          message={{
-            value: "First!",
-            created_at: 2919192,
-            thread_id: "thread_abc123",
-          }}
-        />
-        <ChatMessage
-          isUser={false}
-          message={{
-            value: "Some text here",
-            created_at: 2919192,
-            thread_id: "thread_abc123",
-          }}
-        />
-        <ChatMessage
-          isUser={true}
-          message={{
-            value: "Some text here",
-            created_at: 2919192,
-            thread_id: "thread_abc123",
-          }}
-        />
+        {messages.map((message, idx) => (
+          <ChatMessage
+            key={idx}
+            message={message}
+            isUser={message.role === "user"}
+          />
+        ))}
       </ul>
-      <div className="chat__user-input d-flex flex-row card">
-        <input placeholder="Write something..." />
+      <form
+        className="chat__user-input d-flex flex-row card"
+        onSubmit={(e) => onSubmit(e)}
+      >
+        <input
+          placeholder="Write something..."
+          onChange={(e) => setUserInput(e.target.value)}
+          value={userInput}
+        />
         <button type="submit" className="btn btn-primary">
           Send
         </button>
-      </div>
+      </form>
     </div>
   );
 }
