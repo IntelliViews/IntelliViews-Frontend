4103c96da57c9688d777576dd6d667458c9aaacb
Sabbasn <ben_7aus@hotmail.com>
2024-03-20 21:50:46 +0100

    Move user icon from profile to header

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
+import ProfileIcon from "../common/profile-icon/ProfileIcon";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../App";
 
 export default function Header() {
+  const { userContext } = useContext(AuthContext)!;
+  const { user } = userContext;
+  const [username, setUsername] = useState("Username");
   return (
     <nav className="iv-header">
-      <div className="iv-header--content container">
+      <div className="iv-header--content container justify-content-between">
         <h1 className="iv-header--logo">IntelliViews</h1>
         <div className="nav">
           <Link className="nav-link" to="/">
@@ -16,6 +22,16 @@ export default function Header() {
           <Link className="nav-link" to="/interview">
             Interview
           </Link>
+        </div>
+        <div className="d-flex align-items-center gap-2 justify-content-end">
+          {user ? (
+            <>
+              <ProfileIcon username={username} diameter={40} />
+              <p style={{ margin: "0", opacity: "0.7" }}>{username}</p>
+            </>
+          ) : (
+            <p style={{ margin: "0", opacity: "0.7" }}>Login</p>
+          )}
         </div>
       </div>
     </nav>
--- src/features/profile/components/ratings/ProfileRatingsList.tsx
+++ src/features/profile/components/ratings/ProfileRatingsList.tsx
@@ -3,8 +3,8 @@ import ProfileRatingsPodium from "./ProfileRatingsPodium";
 export default function ProfileRatingsList() {
   return (
     <div className="d-flex flex-row h-100 justify-content-around">
-      <ProfileRatingsPodium rating={10} />
       <ProfileRatingsPodium rating={7} />
+      <ProfileRatingsPodium rating={10} />
       <ProfileRatingsPodium rating={2} />
     </div>
   );
